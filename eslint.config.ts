fb3c12aaf3e3c580a1115b8704feb6a227fe7296
baosal <baoduong55@gmail.com>
2025-10-15 22:38:57 +0700

    1

---
+++ eslint.config.ts
@@ -0,0 +1,23 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
+    settings: {
+      react: {
+        version: "detect", // Automatically detect the React version
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn", // Warn about unused variables
+      "react/react-in-jsx-scope": "off", // Disable React in JSX scope rule
+      "@typescript-eslint/no-explicit-any": "error", // Disallow usage of 'any' in TypeScript
+    },
+  }
+]);
